74d523168565ec8b0c4b79218ee4e8ee92dea70c
James Jilhaney <notail2111@gmail.com>
2022-03-26 09:45:32 +0800

    done formatting landing page

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:bilin/widgets/custom_rounded_button.dart';
 import 'package:bilin/constants/constants_colors.dart';
 import 'package:bilin/constants/constants_text_styles.dart';
-import 'package:bilin/widgets/login_page_card.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:bilin/widgets/login_page_card.dart';
 
 class landingPage extends StatelessWidget {
   const landingPage({Key? key}) : super(key: key);
@@ -62,8 +62,11 @@ class landingPage extends StatelessWidget {
             onPressed: () {},
             child: const Text(
               "Login",
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(fontSize: 20, color: Colors.black),
             ),
+          ),
+          SizedBox(
+            width: 40,
           ),
         ],
       ),
@@ -94,10 +97,10 @@ class landingPage extends StatelessWidget {
                                 style: landingPageBold,
                               ),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
+                            const Padding(
+                              padding: EdgeInsets.symmetric(
                                   vertical: 10, horizontal: 8.0),
-                              child: const Text(
+                              child: Text(
                                 "By empowering those who empower others, we at Bilin are trying to spark the Filipino spirit of Bayanihan like never before",
                                 textAlign: TextAlign.left,
                                 style: landingPageParagraph,
@@ -166,7 +169,7 @@ class landingPage extends StatelessWidget {
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
+                  children: const <Widget>[
                     LandingPageCard(
                       icon: Icons.pin,
                     ),
@@ -187,28 +190,229 @@ class landingPage extends StatelessWidget {
           Container(
             color: Colors.amber,
             height: MediaQuery.of(context).size.height * 1,
-            child: Row(children: [
-              Column(
-                children: <Widget>[
-                  Container(
-                    color: Colors.red,
-                    height: MediaQuery.of(context).size.height * 1,
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    child: Icon(
-                      MaterialCommunityIcons.globe_model,
-                      size: 600,
-                    ),
+            child: Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        color: Colors.red,
+                        height: MediaQuery.of(context).size.height * 1,
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        child: const Icon(
+                          MaterialCommunityIcons.globe_model,
+                          size: 600,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 40.0),
+                        child: Text(
+                          "Our Mission",
+                          style: landingPageBold,
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        child: const Text(
+                          "[Placeholder only] We at Bilin are trying to change this problem, for a better, hunger-free tomorrow.  The Bilin app is a platform that makes the donation process easier, allowing forward-thinking people and businesses to connect with nonprofits and community pantries that need their help the most. We are prioritising the donation of surplus food to tackle two of the most difficult SDGs there are: No Hunger and Food Waste.",
+                          style: landingPageParagraph,
+                          textAlign: TextAlign.justify,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text('19'),
+                                Text("Partnered with Us"),
+                              ],
+                            ),
+                            Column(
+                              children: <Widget>[
+                                Text('19'),
+                                Text("Partnered with Us"),
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 1,
+            color: Colors.green,
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                      ),
+                      Flexible(
+                        child: Center(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.2,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const <Widget>[
+                                Text("Bilin"),
+                                Text(
+                                  'Bilin is a platform that makes the donation process easier, allowing forward-thinking people and businesses to connect with nonprofits and community pantries that need their help the most.',
+                                  style: landingPageParagraph,
+                                  textAlign: TextAlign.justify,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.height * 0.2,
+                      ),
+                      Flexible(
+                        child: Center(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.075,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const <Widget>[
+                                Text(
+                                  "Navigate",
+                                  style: landingPageParagraph,
+                                  textAlign: TextAlign.left,
+                                ),
+                                Text(
+                                  'Home',
+                                  style: landingPageParagraph,
+                                  textAlign: TextAlign.left,
+                                ),
+                                Text(
+                                  'Knowledge HUb',
+                                  style: landingPageParagraph,
+                                  textAlign: TextAlign.left,
+                                ),
+                                Text(
+                                  'About Us',
+                                  style: landingPageParagraph,
+                                  textAlign: TextAlign.left,
+                                ),
+                                Text(
+                                  'FAQ',
+                                  style: landingPageParagraph,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.height * 0.1,
+                      ),
+                      Flexible(
+                        child: Center(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.075,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const <Widget>[
+                                Text(
+                                  'Get Involved',
+                                  style: landingPageParagraph,
+                                ),
+                                Text(
+                                  'Locate',
+                                  style: landingPageParagraph,
+                                ),
+                                Text(
+                                  'Donate',
+                                  style: landingPageParagraph,
+                                ),
+                                Text(
+                                  'Volunteer',
+                                  style: landingPageParagraph,
+                                ),
+                                Text(
+                                  'Initiate',
+                                  style: landingPageParagraph,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.height * 0.1,
+                      ),
+                      Flexible(
+                        child: Center(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.075,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const <Widget>[
+                                Text(
+                                  'Quick Links',
+                                  style: landingPageParagraph,
+                                ),
+                                Text(
+                                  'Food Safety Guidelines',
+                                  style: landingPageParagraph,
+                                ),
+                                Text(
+                                  'Contact Us',
+                                  style: landingPageParagraph,
+                                ),
+                                Text(
+                                  'teamhiraya@gmail.com',
+                                  style: landingPageParagraph,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                  ),
+                  Divider(
+                    color: Colors.white,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text("Lorem Ipsum"),
+                      Text("Lorem Ipsum"),
+                    ],
                   )
                 ],
               ),
-              Column(
-                children: <Widget>[
-                  Text("Title"),
-                  Text("Paragraph"),
-                ],
-              )
-            ]),
-          ),
+            ),
+          )
         ],
       ),
     );
